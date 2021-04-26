095d11cc05b7976e9bebdaa05cf065ff24f054e2
Tarah <tarah.agbokhana@gmail.com>
2021-04-25 23:57:18 -0400

    Initial Login/SignUp Endpoints

---
+++ src/main/java/com/lambdaschool/java/exceptions/ResourceFoundException.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.java.exceptions;
+
+/**
+ * A custom exception to be used when a resource is found but is not suppose to be
+ */
+public class ResourceFoundException
+        extends RuntimeException
+{
+    public ResourceFoundException(String message)
+    {
+        super("Java " + message);
+    }
+}
---
+++ src/main/java/com/lambdaschool/java/repositories/UseremailRepository.java
@@ -0,0 +1,12 @@
+package com.lambdaschool.java.repositories;
+
+import com.lambdaschool.java.models.Useremail;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * The CRUD Repository connecting Useremail to the rest of the application
+ */
+public interface UseremailRepository
+        extends CrudRepository<Useremail, Long>
+{
+}
